a2806ebeb92ebf0967d161cb910891c6bb684294
Thom <thomas.smith@valuecentric.com>
2017-09-19 10:56:44 -0400

    Added base files that don't do anything.

---
+++ index.cfm
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        Hello, World!
+
+        <script src="dist/bundle.js"></script>
+    </body>
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname,
+        filename: './dist/bundle.js',
+    },
+    module: {
+        rules: [],
+    },
+}
